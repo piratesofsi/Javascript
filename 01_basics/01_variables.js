a093c36bf347b9d4d2acda60250f3744ee9d43ea
piratesofsi <146651306+piratesofsi@users.noreply.github.com>
2025-06-24 11:16:14 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,7 @@ const accountId=4343333
 let accountEmail="Musabshaikh88@gmail.com"
 var accountpassword="emilywilliams"
 accountCity="Mumbai"
+accountState
 
 console.log(accountId);
-console.table([accountId,accountEmail,accountpassword,accountCity])
+console.table([accountId,accountEmail,accountpassword,accountCity,accountState])
